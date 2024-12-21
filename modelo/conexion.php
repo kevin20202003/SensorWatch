8785acbf581151fbce97d772330e451391959a46
kevin20202003 <kchala900@gmail.com>
2024-12-21 10:37:37 -0500

    Conectando la base de datos postgres

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -1,6 +1,17 @@
 <?php
 
-$conexion = new mysqli("localhost", "root", "", "invernadero");
-$conexion->set_charset("utf8");
+$host = 'dpg-ctj33hjtq21c73dvtdj0-a'; // External o Internal URL
+$port = '5432'; // Puerto de PostgreSQL
+$dbname = 'invernadero';
+$user = 'invernadero_user';
+$password = 'A01znTdBGmkNia7JhUEmgyMabE90NdCp';
+
+try {
+    $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$dbname", $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Conexión exitosa a PostgreSQL.";
+} catch (PDOException $e) {
+    echo "Error al conectar a PostgreSQL: " . $e->getMessage();
+}
 
 ?>
